9db9a80bbbe53ff86b18164e26b6ec7d479a648e
Woranut Dangworawith <146110465+danworanut@users.noreply.github.com>
2023-10-01 14:43:24 +0700

    update menu bar

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-// hamberger bar toggle
-function toggleHam(x) {
-    x.classList.toggle("change");
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-    let myMenu = document.getElementById('myMenu');
-    if (myMenu.className === 'menu') {
-        myMenu.className += ' menu-active'
-    } else {
-        myMenu.className = 'menu';
-    }
-  }
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-//   scroll section active link 
+window.onscroll = () => {
 
-// let sections = querySelectorAll('section');
-// let navLinks = querySelectorAll('header nav a');
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop - 150;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAtrribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
 
-//         if(top >= offset && top < offset + height) {
-//             navLinks.forEach(links => {
-//                 links.classList.remove('active');
-//                 document.querySelector('heafer nav a[href*=' + id + ']').ClassList.add('active');
-//             });
-//         };
-//     });
+            });
+        }
 
-// }
+    });
+
+
+    //sticky header
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
+
+
